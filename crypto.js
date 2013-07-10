cf93e0a53645710fb351145fd6aa2a02324a9b65
fresheneesz <bitetrudpublic@gmail.com>
2013-07-10 00:38:07 -0700

    reorg crypto and add hmac

--- crypto.js
+++ crypto.js
@@ -1,28 +1,61 @@
 var crypto = require('crypto');
+//var ursa = require('ursa'); // for rsa encryption
 
 exports.hash = function(algorithm, encoding) {
-    var hasher = crypto.createHash(algorithm);
-    this.enc = function(message) {
-        return hasher.update(message).digest(encoding).toString();
-    };
+    return new hasher(algorithm,  encoding);
 };
+exports.hmac = function(algorithm, encoding) {
+    return new hmacer(algorithm,  encoding);
+};
+// todo: test and check out: http://stackoverflow.com/questions/15004951/nodejs-cant-get-crypto-module-to-give-me-the-right-aes-cipher-outcome
 exports.sym = function(algorithm, encoding) {
-    this.enc = function(key, message) {
-        var enc = crypto.createCipher(algorithm, key);
-        return enc.update(message, 'utf8', encoding).toString() + enc.final(encoding);
-    };
-    this.dec = function(key, encryptedMessage) {
-        var dec = crypto.createDecipher(algorithm, key);
-        return dec.update(encryptedMessage, encoding, 'utf8') + enc.final('utf8');
-    };
+    return new symmer(algorithm, encoding);
 };
 exports.ciphers = function() {
     return crypto.getCiphers();
-}
+};
 exports.hashes = function() {
     return crypto.getHashes();
+};
+
+function hasher(algorithm, encoding) {
+    this.hasher = crypto.createHash(algorithm);
+    this.encoding = encoding;
 }
+    hasher.prototype = {
+        enc: function(message) {
+            return this.hasher.update(message).digest(this.encoding).toString();
+        }
+    };
 
-/*asym: function() {
+function hmacer(algorithm, encoding) {
+    this.algorithm = algorithm;
+    this.encoding = encoding;
+}
+    hmacer.prototype = {
+        enc: function(message, key) {
+            var hasher = crypto.createHmac(this.algorithm, key);
+            return hasher.update(message).digest(this.encoding).toString();
+        }
+    };
 
-}*/
+function symmer(algorithm, encoding) {
+    this.algorithm = algorithm;
+    this.encoding = encoding;
+}
+    symmer.prototype = {
+        enc: function(key, message) {
+            var enc = crypto.createCipher(this.algorithm, key);
+            return enc.update(message, 'utf8', this.encoding).toString() + enc.final(this.encoding);
+        },
+        dec: function(key, encryptedMessage) {
+            var dec = crypto.createDecipher(this.algorithm, key);
+            return dec.update(encryptedMessage, this.encoding, 'utf8') + dec.final('utf8');
+        }
+    };
+
+/*
+exports.asym = function() {
+
+};
+*/
